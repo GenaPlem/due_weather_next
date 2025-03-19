379bc904967c3834d00a901d3ee2374e33a204ae
GenaPlem <shainkill1337@gmail.com>
2025-03-19 14:53:25 +0000

    Add header styles

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className={styles.header}>
       <Link href="/">
-        <p className="logo">DueWeather</p>
-        <Image src="/images/logo.svg" alt="logo" width={50} height={50} />
+        <p className={styles.logo}>DueWeather</p>
+        <Image src="/images/logo.svg" alt="logo" width={31} height={31} />
       </Link>
     </header>
   );
