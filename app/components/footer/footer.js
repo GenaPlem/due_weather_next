62564b0b21c1f68ca242d9e16ecb434db3efd38b
GenaPlem <shainkill1337@gmail.com>
2025-03-19 15:03:57 +0000

    Add pathname check to add active class to link

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,17 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import styles from "./footer.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+
   return (
-    <footer className="footer">
+    <footer className={styles.footer}>
       <nav>
-        <ul className="navigation">
-          <li className="navigation__link active">
+        <ul className={styles.navigation}>
+          <li
+            className={`${styles.navigation__link} ${
+              pathname === "/" ? styles.active : ""
+            }`}
+          >
             <Link href="/">Current</Link>
           </li>
-          <li className="navigation__link">
+          <li
+            className={`${styles.navigation__link} ${
+              pathname === "/forecast" ? styles.active : ""
+            }`}
+          >
             <Link href="/forecast">Forecast</Link>
           </li>
-          <li className="navigation__link">
+          <li
+            className={`${styles.navigation__link} ${
+              pathname === "/saved" ? styles.active : ""
+            }`}
+          >
             <Link href="/saved">Saved</Link>
           </li>
         </ul>
